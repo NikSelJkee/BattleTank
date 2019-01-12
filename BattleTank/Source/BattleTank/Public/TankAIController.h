45cb3c912b259229796b000469dbde57d768b085
nikseljkee <nikseljkee1@gmail.com>
2019-01-12 20:21:18 +0300

    Обновлён класс TankAIController. - Добавлена функция нахождения танка игрока.

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "Tank.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 #include "CoreMinimal.h"
 #include "AIController.h"
 #include "TankAIController.generated.h"
@@ -15,8 +17,9 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-public:
-	ATank* GetControlledTank() const;
-
+private:
 	virtual void BeginPlay() override;
+
+	ATank* GetControlledTank() const;
+	ATank* GetPlayerTank() const;
 };
